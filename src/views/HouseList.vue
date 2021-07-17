b3a1278c638b8c62ff98546afa185ddeedc430dc
WangLei <ljl2017027438@126.com>
2021-07-17 18:45:36 +0800

    第三次提交

--- src/views/HouseList.vue
+++ src/views/HouseList.vue
@@ -11,26 +11,26 @@
       <el-main style="padding:0;background-color:#fff;">
         <!-- 筛选按钮 -->
         <div class="queryList">
-          <el-row style="line-height:40px">
-            <el-col :span="3" >户型:</el-col>
-            <el-col :span="2"><el-radio v-model="radio1" label="1" >全部</el-radio></el-col>
-            <el-col :span="2"><el-radio v-model="radio1" label="2" >一室</el-radio></el-col>
-            <el-col :span="2"><el-radio v-model="radio1" label="2" >二室</el-radio></el-col>
-            <el-col :span="2"><el-radio v-model="radio1" label="3" >三室</el-radio></el-col>
-            <el-col :span="2"><el-radio v-model="radio1" label="4" >四室</el-radio></el-col>
+          <el-row style="line-height:40px; font-size: 18px;">
+            <el-col :span="3"> <span>户型:</span> </el-col>
+            <el-col :span="2"><el-radio v-model="radio1" label="1" ><span>全部</span></el-radio></el-col>
+            <el-col :span="2"><el-radio v-model="radio1" label="2" ><span>一室</span></el-radio></el-col>
+            <el-col :span="2"><el-radio v-model="radio1" label="3" ><span>二室</span></el-radio></el-col>
+            <el-col :span="2"><el-radio v-model="radio1" label="4" ><span>三室</span></el-radio></el-col>
+            <el-col :span="2"><el-radio v-model="radio1" label="5" ><span>四室</span></el-radio></el-col>
           </el-row>
           <el-row style="line-height:40px">
-            <el-col :span="3" style="line-height:40px">面积:</el-col>
-            <el-col :span="3"><el-radio v-model="radio2" label="1" >70-90平米</el-radio></el-col>
-            <el-col :span="3"><el-radio v-model="radio2" label="2" >90-110平米</el-radio></el-col>
-            <el-col :span="4"><el-radio v-model="radio2" label="3" >110-130平米</el-radio></el-col>
+            <el-col :span="3" style="line-height:40px"><span>面积:</span></el-col>
+            <el-col :span="3"><el-radio v-model="radio2" label="1" ><span>70-90平米</span></el-radio></el-col>
+            <el-col :span="3"><el-radio v-model="radio2" label="2" ><span>90-110平米</span></el-radio></el-col>
+            <el-col :span="4"><el-radio v-model="radio2" label="3" ><span>110-130平米</span></el-radio></el-col>
           </el-row>
         </div>
         <!-- 内容区域 -->
         <div>
         <!-- 标题 -->
          <div class="details">
-          <p class="title">一共找到<span>10</span>个楼盘</p>
+          <p class="title">共找到<span>10</span>个楼盘</p>
          </div>
          <div class="houseList">
             <div class="houstlist-img">
@@ -38,22 +38,28 @@
             </div>
             <div class="listcenter">
                 <el-row class="row">
-                    <span class="title">上海中集金地美兰城</span>
-                    <div class="price">
-                      <i>约</i><span>44800</span><i>/㎡</i>
+                    <span class="title ">【恒大悦龙台】醇熟配套 金装品质华宅！</span>
+                    <div  style="float: right;">
+                      <i style="color:black;font-size:18px;">约</i>
+                      <span class="price" >44800</span>
+                      <i style="color:black;font-size:18px;">/㎡</i>
                     </div>
                     <p>
-                      <i></i>
-                      上海市宝山区年喜路19弄</p>
+                      <img width="18px" src="../assets/icon/address3.gif" alt="">
+                      上海市宝山区年喜路19弄
+                    </p>
                     <p>
-                      <i></i>
-                      户型：二室/三室/四室</p>
+                      <img width="18px" src="../assets/icon/hall.png" alt="">
+                      户型：二室/三室/四室
+                    </p>
                     <p>
-                      <i></i>
-                      户型：二室/三室/四室</p>
-                    <p>
-                      <i></i>
-                      开盘：无</p>
+                      <img width="18px" src="../assets/icon/activitytime.png" alt="">
+                      开盘：无
+                    </p>
+                    <p> 
+                       <img width="18px" src="../assets/icon/like.gif" alt="">
+                      2345人想买
+                    </p>
                 </el-row>
             </div>
          </div>
@@ -81,11 +87,15 @@
 </script>
 
 <style scoped>
+.listcenter{
+  padding-top: 20px;;padding-left:418px ;
+}
 .row{
   text-align: left;
 }
 .price{
-  float: right;
+  display: inline-block;
+  margin-top: 20px;margin-right: 0.3125rem;margin-bottom: 0.3125rem; margin-left: 0.3125rem;font-size: 31.75px;color: #ee722e;
 }
 .houseList{
   padding-top: 40px;
@@ -125,18 +135,23 @@
     margin-left: 0.625rem;
 }
 .title{
+  
   width: 1080px;
   text-align: left;
   margin: 0 auto;
   margin-top:30px ;
   margin-bottom:30px;
-  font-size: 26px;
+  font-size: 31.5px;
 }
 .queryList{
+
  width: 1200px;
  margin: 0 auto;
  margin-top:43px;
  border-bottom: .0625rem solid #eee;
+}
+.queryList span{
+  font-size: 18px;
 }
 .search{
     width: 50%;
