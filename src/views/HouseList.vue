8382f0f4ea756a01e3fe82131b4a0aecc4784eee
WangLei <ljl2017027438@126.com>
2021-07-17 10:26:58 +0800

    第二次提交，完善list页面

--- src/views/HouseList.vue
+++ src/views/HouseList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <el-container>
+        <!-- 搜索栏 -->
       <el-header class="header" height='114px' style="background-color:#fdf1ea">  
        <div class="hcontent">
           <el-input class="search" placeholder="您可以输入楼盘的名称" suffix-icon='el-icon-search' v-model="searchval"></el-input>
@@ -8,6 +9,7 @@
        </div>
       </el-header>
       <el-main style="padding:0;background-color:#fff;">
+        <!-- 筛选按钮 -->
         <div class="queryList">
           <el-row style="line-height:40px">
             <el-col :span="3" >户型:</el-col>
@@ -24,15 +26,35 @@
             <el-col :span="4"><el-radio v-model="radio2" label="3" >110-130平米</el-radio></el-col>
           </el-row>
         </div>
+        <!-- 内容区域 -->
         <div>
+        <!-- 标题 -->
          <div class="details">
           <p class="title">共找到<span>10</span>个楼盘</p>
          </div>
          <div class="houseList">
-            <div>
-             
-                <img src="../assets/houseList/zhongjijindimeicheng.png" alt="">
-             
+            <div class="houstlist-img">
+                <img class="banner" src="../assets/houseList/zhongjijindimeicheng.png" alt="">
+            </div>
+            <div class="listcenter">
+                <el-row class="row">
+                    <span class="title">上海中集金地美兰城</span>
+                    <div class="price">
+                      <i>约</i><span>44800</span><i>/㎡</i>
+                    </div>
+                    <p>
+                      <i></i>
+                      上海市宝山区年喜路19弄</p>
+                    <p>
+                      <i></i>
+                      户型：二室/三室/四室</p>
+                    <p>
+                      <i></i>
+                      户型：二室/三室/四室</p>
+                    <p>
+                      <i></i>
+                      开盘：无</p>
+                </el-row>
             </div>
          </div>
         </div>
@@ -59,10 +81,38 @@
 </script>
 
 <style scoped>
+.row{
+  text-align: left;
+}
+.price{
+  float: right;
+}
+.houseList{
+  padding-top: 40px;
+  padding-bottom: 40px;
+}
+.houstlist-img{
+  position: relative;
+  float: left;
+  height: 14.375rem;
+  width: 23.75rem;
+  overflow: hidden;
+}
+.banner{
+  float: left;
+    width: 100%;
+    /* width: auto; */
+    height: auto;
+    max-width: 100%;
+    max-height: 100%;
+    min-width: 100%;
+    min-height: 100%;
+    object-fit: cover;
+}
 .houseList{
   width: 1200px;
   margin: 0 auto;
-  border: 1px solid black;
+  /* border: 1px solid black; */
 }
 .details{
   margin: 0 auto;
@@ -80,7 +130,6 @@
   margin: 0 auto;
   margin-top:30px ;
   margin-bottom:30px;
-  padding-left:40px ;
   font-size: 26px;
 }
 .queryList{
